234f87ff1bcea9b28e2cdf3702bca5291293549e
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 13:16:08 +0200

    Create new value object

---
+++ POKA.POC.DDD/src/POKA.POC.DDD.Domain/ValueObjects/MenuItemId.cs
@@ -0,0 +1,14 @@
+﻿namespace POKA.POC.DDD.Domain.ValueObjects
+{
+    public record MenuItemId : BaseObjectId
+    {
+        protected override string _type => "menuItem";
+
+        public MenuItemId(Guid value)
+            : base(value)
+        {
+        }
+
+        public override string ToString() => base.ToString();
+    }
+}
