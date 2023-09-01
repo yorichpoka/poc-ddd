b65b2878d596fdaf674a77090a57efb08934d4cc
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 12:56:04 +0200

    Create new entity class

---
+++ POKA.POC.DDD/src/POKA.POC.DDD.Domain/Entities/MenuSectionEntity.cs
@@ -0,0 +1,37 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+
+namespace POKA.POC.DDD.Domain.Entities
+{
+    public class MenuSectionEntity : BaseEntity<MenuSectionId>
+    {
+        public MenuId MenuId { get; private set; } = null!;
+        public string Name { get; private set; } = null!;
+        public string Description { get; private set; } = null!;
+
+        public MenuSectionEntity()
+        {
+        }
+
+        public MenuSectionEntity(MenuId menuId, string name, string description)
+        {
+            if (menuId.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(menuId));
+            }
+
+            if (name.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(name));
+            }
+
+            if (description.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(description));
+            }
+
+            Description = description;
+            MenuId = menuId;
+            Name = name;
+        }
+    }
+}
