6e2c76706e437b807e9076eb9242bf72dff67c3d
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 14:47:08 +0200

    Create domain event

---
+++ src/POKA.POC.DDD.Domain/DomainEvents/BaseDomainEvent.cs
@@ -0,0 +1,31 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+using POKA.POC.DDD.Domain.Interfaces;
+using POKA.POC.DDD.Domain.Exceptions;
+using POKA.POC.DDD.Domain.Enums;
+
+namespace POKA.POC.DDD.Domain.DomainEvents
+{
+    public abstract record BaseDomainEvent<TObjectId> : IDomainEvent<TObjectId>
+        where TObjectId : BaseObjectId
+    {
+        public TObjectId Id { get; protected set; } = null!;
+        public UserId? AuthorId { get; protected set; }
+        public int Version { get; protected set; }
+
+        protected BaseDomainEvent(TObjectId id, int version, UserId? authorId = null)
+        {
+            if (authorId != null && authorId.Value == default)
+            {
+                throw new AppException(AppErrorEnum.ArgumentNullPassed, nameof(authorId));
+            }
+
+            AuthorId = authorId;
+            Version = version;
+            Id = id;
+        }
+
+        public void AssignId(TObjectId id) => this.Id = id;
+
+        public string GetStreamName() => $"{this.GetType().Name}-{this.Id}";
+    }
+}
---
+++ src/POKA.POC.DDD.Domain/DomainEvents/MenuAggregate/MenuCreated.cs
@@ -0,0 +1,37 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+using POKA.POC.DDD.Domain.Exceptions;
+
+namespace POKA.POC.DDD.Domain.DomainEvents
+{
+    public record MenuCreated : BaseDomainEvent<MenuId>
+    {
+        public HostId HostId { get; private set; } = null!;
+        public string Name { get; private set; } = null!;
+        public string Description { get; private set; } = null!;
+        public DateTime On { get; private set; }
+
+        public MenuCreated(MenuId id, HostId hostId, string name, string description, DateTime on, UserId? authorId = null)
+            : base(id, 0, authorId)
+        {
+            if (hostId.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(hostId));
+            }
+
+            if (name.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(name));
+            }
+
+            if (description.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(description));
+            }
+
+            Description = description;
+            HostId = hostId;
+            Name = name;
+            On = on;
+        }
+    }
+}
