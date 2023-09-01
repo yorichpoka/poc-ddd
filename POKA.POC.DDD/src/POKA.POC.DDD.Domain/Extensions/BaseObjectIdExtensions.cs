360863ed0cd7e1d95ab188db29476968b7964b4a
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 12:54:38 +0200

    Create new extension methods

---
+++ POKA.POC.DDD/src/POKA.POC.DDD.Domain/Extensions/BaseObjectIdExtensions.cs
@@ -0,0 +1,9 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+
+namespace POKA.POC.DDD.Extensions
+{
+    public static class BaseObjectIdExtensions
+    {
+        public static bool HasValue(this BaseObjectId value) => value != null && value.Value != default;
+    }
+}
