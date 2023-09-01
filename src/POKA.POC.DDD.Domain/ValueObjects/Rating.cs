acdac9527c278c159f7afa4ee256ffe6ae95913a
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 17:36:26 +0200

    Improve value object

--- src/POKA.POC.DDD.Domain/ValueObjects/Rating.cs
+++ src/POKA.POC.DDD.Domain/ValueObjects/Rating.cs
@@ -1,19 +1,22 @@
 ﻿using POKA.POC.DDD.Domain.Exceptions;
+using POKA.POC.DDD.Domain.Interfaces;
 
 namespace POKA.POC.DDD.Domain.ValueObjects
 {
-    public record Rating
+    public record Rating : IHasCreatedOn
     {
         public UserId UserId { get; private set; } = null!;
         public float Score { get; private set; }
+        public DateTime CreatedOn { get; private set; }
 
-        public Rating(UserId userId, float score)
+        public Rating(UserId userId, float score, DateTime createdOn)
         {
             if (userId.HasValue() == false)
             {
                 throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(userId));
             }
 
+            CreatedOn = createdOn;
             UserId = userId;
             Score = score;
         }
