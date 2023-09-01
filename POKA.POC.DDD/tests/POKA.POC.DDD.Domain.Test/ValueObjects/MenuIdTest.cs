fe8e111ff725441dc0deb31eed479e79598ec354
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 13:11:06 +0200

    Create test class

---
+++ POKA.POC.DDD/tests/POKA.POC.DDD.Domain.Test/ValueObjects/MenuIdTest.cs
@@ -0,0 +1,22 @@
+﻿namespace POKA.POC.DDD.Domain.Test.ValueObjects
+{
+    public class MenuIdTest
+    {
+        [Fact]
+        public void CanHaveToString()
+        {
+            // A
+            var menuId = BaseObjectId.Create<MenuId>();
+
+            // A
+            var stringValue = menuId.ToString();
+            var typeName = menuId.GetTypeName();
+
+            // A
+            Assert.NotNull(stringValue);
+            Assert.NotNull(typeName);
+            Assert.True(stringValue.Contains(menuId.Value.ToString()));
+            Assert.True(stringValue.Contains(typeName));
+        }
+    }
+}
