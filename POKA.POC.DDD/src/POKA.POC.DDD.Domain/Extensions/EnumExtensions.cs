f1293742cf46ab7a9c85d2f173c15c1bba4b338a
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 12:32:13 +0200

    Create extension method

---
+++ POKA.POC.DDD/src/POKA.POC.DDD.Domain/Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace POKA.POC.DDD.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription<TEnum>(this TEnum value) where TEnum : Enum
+        {
+            var fieldInfo = value.GetType()
+                                 .GetField($"{value}");
+            var descriptionAttribute = fieldInfo.GetCustomAttribute<DescriptionAttribute>();
+
+            if (descriptionAttribute is null)
+            {
+                return $"{value}";
+            }
+
+            return descriptionAttribute.Description;
+        }
+    }
+}
