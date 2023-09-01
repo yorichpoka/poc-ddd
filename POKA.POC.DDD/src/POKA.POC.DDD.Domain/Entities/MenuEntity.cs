e8f73c506ae10579943f76073899894533a70943
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 12:47:24 +0200

    Create new entity class

---
+++ POKA.POC.DDD/src/POKA.POC.DDD.Domain/Entities/MenuEntity.cs
@@ -0,0 +1,25 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+
+namespace POKA.POC.DDD.Domain.Entities
+{
+    public class MenuEntity : BaseEntity<MenuId>
+    {
+        public HostId HostId { get; private set; } = null!;
+        public string Name { get; private set; } = null!;
+        public string Description { get; private set; } = null!;
+        public DateTime CreatedOn { get; private set; }
+        public DateTime? LastUpdatedOn { get; private set; } = null;
+
+        public MenuEntity()
+        {
+        }
+
+        public MenuEntity(HostId hostId, string name, string description)
+        {
+            CreatedOn = DateTime.UtcNow;
+            Description = description;
+            HostId = hostId;
+            Name = name;
+        }
+    }
+}
