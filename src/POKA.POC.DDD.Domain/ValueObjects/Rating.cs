fbe2b962977970dfad1e31a5eeaff363d16fe9d4
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 14:08:08 +0200

    Create new value objects

---
+++ src/POKA.POC.DDD.Domain/ValueObjects/Rating.cs
@@ -0,0 +1,23 @@
+﻿using POKA.POC.DDD.Domain.Exceptions;
+
+namespace POKA.POC.DDD.Domain.ValueObjects
+{
+    public record Rating
+    {
+        public UserId UserId { get; private set; } = null!;
+        public float Score { get; private set; }
+
+        public Rating(UserId userId, float score)
+        {
+            if (userId.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(userId));
+            }
+
+            UserId = userId;
+            Score = score;
+        }
+
+        public override string ToString() => $"{this.UserId} ({this.Score})";
+    }
+}
