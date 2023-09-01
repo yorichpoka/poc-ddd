8469794b7fbb38d7210371268e6ff9ee84b61375
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 17:37:01 +0200

    Create new domain event class

---
+++ src/POKA.POC.DDD.Domain/DomainEvents/MenuAggregate/MenuRated.cs
@@ -0,0 +1,25 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+using POKA.POC.DDD.Domain.Exceptions;
+
+namespace POKA.POC.DDD.Domain.DomainEvents
+{
+    public record MenuRated : BaseDomainEvent<MenuId>
+    {
+        public UserId UserId { get; private set; } = null!;
+        public float Score { get; private set; }
+        public DateTime On { get; private set; }
+
+        public MenuRated(MenuId id, int version, UserId userId, float score, DateTime on, UserId? authorId = null)
+            : base(id, version, authorId)
+        {
+            if (userId.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(userId));
+            }
+
+            UserId = userId;
+            Score = score;
+            On = on;
+        }
+    }
+}
