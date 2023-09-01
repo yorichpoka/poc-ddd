2663b9149128bc3e2e2f383374dac050d8bee6b9
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 14:46:41 +0200

    Improve aggregate

--- src/POKA.POC.DDD.Domain/Aggregates/MenuAggregate.cs
+++ src/POKA.POC.DDD.Domain/Aggregates/MenuAggregate.cs
@@ -1,6 +1,9 @@
-﻿using POKA.POC.DDD.Domain.ValueObjects;
+﻿using POKA.POC.DDD.Domain.DomainEvents;
+using POKA.POC.DDD.Domain.ValueObjects;
 using POKA.POC.DDD.Domain.Interfaces;
 using System.Collections.ObjectModel;
+using POKA.POC.DDD.Domain.Exceptions;
+using POKA.POC.DDD.Domain.Enums;
 
 namespace POKA.POC.DDD.Domain.Aggregates
 {
@@ -12,19 +15,70 @@ namespace POKA.POC.DDD.Domain.Aggregates
         public HostId HostId { get; private set; } = null!;
         public string Name { get; private set; } = null!;
         public string Description { get; private set; } = null!;
+        public UserId? CreatedByUserId { get; private set; } = null;
         public DateTime? LastUpdatedOn { get; private set; } = null;
 
-        public float GetAverageRatings() => this._ratings
-                                                .Select(l => l.Score)
-                                                .Average();
+        public float GetAverageRatings() => (
+            this._ratings
+                .Select(l => l.Score)
+                .Average()
+        );
 
-        public ReadOnlyCollection<MenuSectionId> GetMenuSections() =>   this._menuSections
-                                                                            .ToList()
-                                                                            .AsReadOnly();
+        public ReadOnlyCollection<MenuSectionId> GetMenuSections() => (
+            this._menuSections
+                .ToList()
+                .AsReadOnly()
+        );
+
+        public static MenuAggregate Create(HostId hostId, string name, string description, UserId? authorId = null)
+        {
+            if (hostId.HasValue() == false)
+            {
+                throw new AppException(AppErrorEnum.ArgumentNullPassed, nameof(hostId));
+            }
+
+            if (name.HasValue() == false)
+            {
+                throw new AppException(AppErrorEnum.ArgumentNullPassed, nameof(name));
+            }
+
+            if (description.HasValue() == false)
+            {
+                throw new AppException(AppErrorEnum.ArgumentNullPassed, nameof(description));
+            }
+
+            var menuId = BaseObjectId.Create<MenuId>();
+            var menuAggregate = new MenuAggregate();
+
+            var domainEvent = new MenuCreated(menuId, hostId, name, description, DateTime.UtcNow, authorId);
+
+            menuAggregate.ApplyUncommittedDomainEvent(domainEvent);
+
+            return menuAggregate;
+        }
 
         public override void ApplyDomainEventImplementation(IDomainEvent<MenuId> domainEvent)
         {
-            throw new NotImplementedException();
+            switch (domainEvent)
+            {
+                #region MenuCreated
+
+                case MenuCreated e:
+                    this.Description = e.Description;
+                    this.HostId = e.HostId;
+                    this.Name = e.Name;
+
+                    this.CreatedByUserId = e.AuthorId;
+                    this.CreatedOn = e.On;
+                    this.Version = e.Version;
+                    this.Id = e.Id;
+                    break;
+
+                #endregion
+
+                default:
+                    throw new AppException(AppErrorEnum.NotImplemented, nameof(ApplyDomainEventImplementation));
+            }
         }
 
         public override void Snapshot()
