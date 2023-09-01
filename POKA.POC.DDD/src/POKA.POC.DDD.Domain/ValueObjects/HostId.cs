2cd1cb8b411d8ee80bb7bca5607486dcc91813aa
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 12:44:34 +0200

    Create new object ids

---
+++ POKA.POC.DDD/src/POKA.POC.DDD.Domain/ValueObjects/HostId.cs
@@ -0,0 +1,14 @@
+﻿namespace POKA.POC.DDD.Domain.ValueObjects
+{
+    public record HostId : BaseObjectId
+    {
+        protected override string _type => "host";
+
+        public HostId(Guid value)
+            : base(value)
+        {
+        }
+
+        public override string ToString() => base.ToString();
+    }
+}
