91a07860dddfa06c4963a9212c2bd4bd795991f8
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-01 12:23:06 +0200

    Create class 'Constants'

---
+++ POKA.POC.DDD/src/POKA.POC.DDD.Domain/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace POKA.POC.DDD.Domain
+{
+    public static class Constants
+    {
+        public static readonly JsonSerializerSettings DefaultJsonSerializerSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.None,
+            MaxDepth = 3
+        };
+    }
+}
