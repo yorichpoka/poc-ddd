a164797d273b6fbbd921f8d06622665f27490df9
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-02 12:35:25 +0200

    Create new value object

---
+++ src/POKA.POC.DDD.Domain/ValueObjects/BasePerson.cs
@@ -0,0 +1,33 @@
+﻿using POKA.POC.DDD.Domain.Exceptions;
+using POKA.POC.DDD.Domain.Enums;
+
+namespace POKA.POC.DDD.Domain.ValueObjects
+{
+    public abstract record BasePerson<TObjectId>
+        where TObjectId : BaseObjectId
+    {
+        public TObjectId Id { get; protected set; } = null!;
+        public string FirstName { get; protected set; } = null!;
+        public string LastName { get; protected set; } = null!;
+        public string? Email { get; protected set; } = null!;
+        public Address? Address { get; protected set; } = null;
+        public DateTime? BornOn { get; protected set; } = null;
+
+        protected BasePerson(TObjectId id, string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+        }
+
+        public void ChangeAddress(Address value)
+        {
+            if (value == null)
+            {
+                throw new AppException(AppErrorEnum.ArgumentNullPassed, nameof(value));
+            }
+
+            this.Address = value;
+        }
+    }
+}
