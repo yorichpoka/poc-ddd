a37ef13848b09f7369b8aba2796a5a412f210e3c
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-02 12:44:11 +0200

    Create new entity class

---
+++ src/POKA.POC.DDD.Domain/Entities/StudentEntity.cs
@@ -0,0 +1,21 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+using POKA.POC.DDD.Domain.Interfaces;
+
+namespace POKA.POC.DDD.Domain.Entities
+{
+    public class StudentEntity : BasePersonEntity<StudentId>, IHasCreatedOn
+    {
+        public DateTime CreatedOn { get; private set; }
+
+        public StudentEntity()
+            : base()
+        {
+        }
+
+        public StudentEntity(StudentId id, string firstName, string lastName) 
+            : base(id, firstName, lastName)
+        {
+            CreatedOn = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/POKA.POC.DDD.Domain/ValueObjects/ObjectIds/StudentId.cs
@@ -0,0 +1,14 @@
+﻿namespace POKA.POC.DDD.Domain.ValueObjects
+{
+    public record StudentId : BaseObjectId
+    {
+        protected override string _type => "student";
+
+        public StudentId(Guid value)
+            : base(value)
+        {
+        }
+
+        public override string ToString() => base.ToString();
+    }
+}
