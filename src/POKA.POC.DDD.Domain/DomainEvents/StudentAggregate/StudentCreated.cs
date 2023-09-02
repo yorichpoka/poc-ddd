7e6b7f50b18ee6b10e007a1b048349f2e6675c9c
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-02 13:07:28 +0200

    Create doman event

---
+++ src/POKA.POC.DDD.Domain/DomainEvents/StudentAggregate/StudentCreated.cs
@@ -0,0 +1,34 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+using POKA.POC.DDD.Domain.Exceptions;
+
+namespace POKA.POC.DDD.Domain.DomainEvents
+{
+    public record StudentCreated : BaseDomainEvent<StudentId>
+    {
+        public string FirstName { get; protected set; } = null!;
+        public string LastName { get; protected set; } = null!;
+        public string? Email { get; protected set; } = null!;
+        public Address? Address { get; protected set; } = null;
+        public DateTime? BornOn { get; protected set; } = null;
+
+        public StudentCreated(StudentId id, string firstName, string lastName, string? email, Address? address, DateTime? bornOn, UserId? authorId = null)
+            : base(id, 0, authorId)
+        {
+            if (firstName.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(firstName));
+            }
+
+            if (lastName.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(lastName));
+            }
+
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            BornOn = bornOn;
+            Email = email;
+        }
+    }
+}
