5d090fa7939dd3aa37818b17f86bfc41d5adab97
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-02 12:47:42 +0200

    Create new class entity

---
+++ src/POKA.POC.DDD.Domain/Entities/CourseEntity.cs
@@ -0,0 +1,38 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+using POKA.POC.DDD.Domain.Exceptions;
+
+namespace POKA.POC.DDD.Domain.Entities
+{
+    public class CourseEntity : BaseEntity<CourseId>
+    {
+        public string Name { get; private set; } = null!;
+        public string Description { get; private set; } = null!;
+        public short Coefficient { get; private set; }
+
+        public CourseEntity()
+        {
+        }
+
+        public CourseEntity(string name, string description, short coefficient)
+        {
+            if (name.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(name));
+            }
+
+            if (description.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(description));
+            }
+
+            if (coefficient <= 0)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(coefficient));
+            }
+
+            Coefficient = coefficient;
+            Description = description;
+            Name = name;
+        }
+    }
+}
---
+++ src/POKA.POC.DDD.Domain/ValueObjects/ObjectIds/CourseId.cs
@@ -0,0 +1,14 @@
+﻿namespace POKA.POC.DDD.Domain.ValueObjects
+{
+    public record CourseId : BaseObjectId
+    {
+        protected override string _type => "course";
+
+        public CourseId(Guid value)
+            : base(value)
+        {
+        }
+
+        public override string ToString() => base.ToString();
+    }
+}
