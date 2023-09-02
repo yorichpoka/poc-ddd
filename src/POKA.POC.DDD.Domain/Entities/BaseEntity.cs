ca1d4af25e943cef577e3440a9397e6ed79886cd
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-02 12:39:51 +0200

    Make class as record

--- src/POKA.POC.DDD.Domain/Entities/BaseEntity.cs
+++ src/POKA.POC.DDD.Domain/Entities/BaseEntity.cs
@@ -2,48 +2,12 @@
 
 namespace POKA.POC.DDD.Domain.Entities
 {
-    public abstract class BaseEntity<TObjectId> : IEntity where TObjectId : class, IObjectId
+    public abstract record BaseEntity<TObjectId> : IEntity where TObjectId : class, IObjectId
     {
         public TObjectId Id { get; protected set; } = null!;
 
-        public override bool Equals(object obj)
-        {
-            var compareTo = obj as BaseEntity<TObjectId>;
+        public override int GetHashCode() => (GetType().GetHashCode() * 907) + Id.GetHashCode();
 
-            if (ReferenceEquals(this, compareTo))
-            {
-                return true;
-            }
-
-            if (ReferenceEquals(null, compareTo))
-            {
-                return false;
-            }
-
-            return Id.Equals(compareTo.Id);
-        }
-
-        public static bool operator ==(BaseEntity<TObjectId> a, BaseEntity<TObjectId> b)
-        {
-            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
-            {
-                return true;
-            }
-
-            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
-            {
-                return false;
-            }
-
-            return a.Equals(b);
-        }
-
-        public static bool operator !=(BaseEntity<TObjectId> a, BaseEntity<TObjectId> b) => !(a == b);
-
-        public override int GetHashCode() => 
-            (GetType().GetHashCode() * 907) + Id.GetHashCode();
-
-        public override string ToString() => 
-            $"{GetType().Name} [Id={Id}]";
+        public override string ToString() => $"{GetType().Name} [Id={Id}]";
     }
 }
