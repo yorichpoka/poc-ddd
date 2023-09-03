261191e8a3a1c819cbabe843d19c450c515dbb69
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-03 20:41:48 +0200

    Create new class 'ExamEntity'

---
+++ src/POKA.POC.DDD.Domain/Entities/ExamEntity.cs
@@ -0,0 +1,34 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+using POKA.POC.DDD.Domain.Exceptions;
+using POKA.POC.DDD.Domain.Enums;
+
+namespace POKA.POC.DDD.Domain.Entities
+{
+    public class ExamEntity : BaseEntity<ExamId>
+    {
+        public string Code { get; private set; } = null!;
+        public string Name { get; private set; } = null!;
+        public string? Description { get; private set; }
+
+        public ExamEntity()
+        {
+        }
+
+        public ExamEntity(string code, string name, string? description = null)
+        {
+            if (name.HasValue() == false)
+            {
+                throw new AppException(AppErrorEnum.ArgumentNullPassed, nameof(name));
+            }
+
+            if (code.HasValue() == false)
+            {
+                throw new AppException(AppErrorEnum.ArgumentNullPassed, nameof(code));
+            }
+
+            Description = description;
+            Code = code;
+            Name = name;
+        }
+    }
+}
---
+++ src/POKA.POC.DDD.Domain/ValueObjects/ObjectIds/ExamId.cs
@@ -0,0 +1,14 @@
+﻿namespace POKA.POC.DDD.Domain.ValueObjects
+{
+    public record ExamId : BaseObjectId
+    {
+        protected override string _type => "exam";
+
+        public ExamId(Guid value)
+            : base(value)
+        {
+        }
+
+        public override string ToString() => base.ToString();
+    }
+}
