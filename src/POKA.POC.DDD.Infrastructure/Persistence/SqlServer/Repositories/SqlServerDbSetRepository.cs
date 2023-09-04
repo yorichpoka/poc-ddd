e969ad926a4528fbb862b3d2b54e6210b36575e6
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 12:24:08 +0200

    Create implementation of IRepositories

---
+++ src/POKA.POC.DDD.Infrastructure/Persistence/SqlServer/Repositories/SqlServerDbSetRepository.cs
@@ -0,0 +1,46 @@
+﻿using POKA.POC.DDD.Infrastructure.Persistence.SqlServer.DbContexts;
+
+namespace POKA.POC.DDD.Infrastructure.Persistence.SqlServer.Repositories
+{
+    public class SqlServerDbSetRepository<TEntity> : SqlServerRepositoryQueryableImplementation<TEntity, SqlMasterDbContext>, IDbSetRepository<TEntity> 
+        where TEntity : class, IEntity
+    {
+        public SqlServerDbSetRepository(SqlMasterDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+
+        public async Task DeleteAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
+        {
+            var entities =  await
+                                this._dbSet
+                                    .Where(predicate)
+                                    .ToListAsync(cancellationToken);
+
+            this._dbSet.RemoveRange(entities);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<TEntity[]> CreateRangeAsync(TEntity[] entities, CancellationToken cancellationToken = default)
+        {
+            await this._dbSet.AddRangeAsync(entities, cancellationToken);
+
+            await this._dbContext.SaveChangesAsync(cancellationToken);
+
+            return entities;
+        }
+
+        public Task UpdateAsync(IObjectId id, TEntity entity, CancellationToken cancellationToken = default) =>
+            this._dbContext.SaveChangesAsync(cancellationToken);
+
+        public async Task<TEntity> CreateAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            await this._dbSet.AddAsync(entity, cancellationToken);
+
+            await this._dbContext.SaveChangesAsync(cancellationToken);
+
+            return entity;
+        }
+    }
+}
