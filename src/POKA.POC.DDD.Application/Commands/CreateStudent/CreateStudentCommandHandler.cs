2e7a881a239be81a09c0f99431b652633ed19f47
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 11:46:01 +0200

    Improve handler of command

--- src/POKA.POC.DDD.Application/Commands/CreateStudent/CreateStudentCommandHandler.cs
+++ src/POKA.POC.DDD.Application/Commands/CreateStudent/CreateStudentCommandHandler.cs
@@ -1,15 +1,16 @@
 ﻿using POKA.POC.DDD.Application.Interfaces;
-using POKA.POC.DDD.Domain.Enums;
 
 namespace POKA.POC.DDD.Extensions.Commands
 {
     public class CreateStudentCommandHandler : IRequestHandler<CreateStudentCommand, StudentId>
     {
+        private readonly ICurrentUserProvider _currentUserProvider;
         private readonly IMasterDbRepository _masterDbRepository;
         private readonly IMediator _mediator;
 
-        public CreateStudentCommandHandler(IMasterDbRepository masterDbRepository, IMediator mediator)
+        public CreateStudentCommandHandler(ICurrentUserProvider currentUserProvider, IMasterDbRepository masterDbRepository, IMediator mediator)
         {
+            _currentUserProvider = currentUserProvider;
             _masterDbRepository = masterDbRepository;
             _mediator = mediator;
         }
@@ -28,6 +29,7 @@ namespace POKA.POC.DDD.Extensions.Commands
 
             var studentAggregate =  StudentAggregate
                                         .Create(
+                                            authorId: this._currentUserProvider.Id,
                                             firstName: request.FirstName,
                                             lastName: request.LastName,
                                             bornOn: request.BornOn,
