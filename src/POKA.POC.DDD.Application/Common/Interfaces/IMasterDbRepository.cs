52f53e50e62c4661d7c372ca416ead7447dadf2b
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 10:47:46 +0200

    Create new interface

---
+++ src/POKA.POC.DDD.Application/Common/Interfaces/IMasterDbRepository.cs
@@ -0,0 +1,11 @@
+﻿using POKA.POC.DDD.Domain.Entities;
+
+namespace POKA.POC.DDD.Application.Interfaces
+{
+    public interface IMasterDbRepository : IUnitOfWork
+    {
+        IDbSetRepository<StudentAggregate> Students { get; }
+        IDbSetRepository<CourseEntity> Courses { get; }
+        IDbSetRepository<ExamEntity> Exams { get; }
+    }
+}
