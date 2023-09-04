9a1952589a7b499649290d74abaa0d9c906750e7
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 11:54:14 +0200

    Create new behaviors and update global using

---
+++ src/POKA.POC.DDD.Application/Common/Behaviors/LoggingPipelineBehavior.cs
@@ -0,0 +1,35 @@
+﻿namespace POKA.POC.DDD.Application.Behaviors
+{
+    public class LoggingPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingPipelineBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingPipelineBehavior(ILogger<LoggingPipelineBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var requestName = request.GetType().Name;
+            TResponse? result;
+
+            this._logger.LogInformation($"'{requestName}' is running.");
+
+            try
+            {
+                result = await next();
+
+                this._logger.LogInformation($"'{requestName}' is done with success.");
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogInformation($"'{requestName}' is done with error, {ex.StackTrace}.");
+
+                throw;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/POKA.POC.DDD.Application/Common/Behaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,24 @@
+﻿namespace POKA.POC.DDD.Application.Behaviors
+{
+    public class ValidationPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IValidator<TRequest>? _validator;
+
+        public ValidationPipelineBehavior(IValidator<TRequest>? validator = null)
+        {
+            _validator = validator;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (this._validator is not null)
+            {
+                await this._validator.ValidateAndThrowAsync(request, cancellationToken);
+            }
+
+            var result = await next();
+
+            return result;
+        }
+    }
+}
