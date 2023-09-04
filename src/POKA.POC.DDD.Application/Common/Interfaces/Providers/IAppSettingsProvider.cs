6683017521065369716879effcecedf3d2aa3b55
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 11:28:17 +0200

    Create new interface

---
+++ src/POKA.POC.DDD.Application/Common/Interfaces/Providers/IAppSettingsProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace POKA.POC.DDD.Application.Common.Interfaces.Providers
+{
+    public interface IAppSettingsProvider
+    {
+        EnvironmentEnum EnvironmentName { get; }
+        string SqlDbConnectionString { get; }
+    }
+}
