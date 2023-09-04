ee50b07b84a3ffdeb8fe29bf53bd0e57062c9442
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 11:30:43 +0200

    Create new interface

---
+++ src/POKA.POC.DDD.Domain/Enums/EnvironmentEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace POKA.POC.DDD.Domain.Enums
+{
+    public sealed class EnvironmentEnum : BaseEnum<EnvironmentEnum>
+    {
+        public static readonly EnvironmentEnum Development = new(0, "Dev");
+        public static readonly EnvironmentEnum Staging = new(1, "Staging");
+        public static readonly EnvironmentEnum Prod = new(2, "Production");
+
+        public EnvironmentEnum(int value, string name)
+            : base(value, name)
+        {
+        }
+    }
+}
