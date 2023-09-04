78d9f36117bdb70cc278dfe7535351f5fccec78d
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 15:34:44 +0200

    Clean class Program

--- src/POKA.POC.DDD.Application.ConsoleApp/Program.cs
+++ src/POKA.POC.DDD.Application.ConsoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace POKA.POC.DDD.Application.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
