0232226a793756e31328d1e66b2cc01ff8c69874
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 12:04:39 +0200

    Create new implementation of 'IAppSettingsProvider'

---
+++ src/POKA.POC.DDD.Infrastructure/Providers/AppSettingsProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace POKA.POC.DDD.Infrastructure.Providers
+{
+    public class AppSettingsProvider : IAppSettingsProvider
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppSettingsProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public EnvironmentEnum EnvironmentName => EnvironmentEnum.FromValue(this._configuration.GetValue<string>("EnvironmentName"));
+
+        public string SqlDbConnectionString => this._configuration.GetValue<string>("ConnectionStrings:SqlServer");
+    }
+}
