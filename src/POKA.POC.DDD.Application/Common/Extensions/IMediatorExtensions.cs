8c09563cb034a6d6da2e430a0d6912828cdea227
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 11:14:48 +0200

    Create new extensions methods

---
+++ src/POKA.POC.DDD.Application/Common/Extensions/IMediatorExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace POKA.POC.DDD.Extensions
+{
+    public static class IMediatorExtensions
+    {
+        public static async Task PublishAsync<TObjectId>(this IMediator mediator, IDomainEvent<TObjectId> domainEvent, CancellationToken cancellationToken = default) 
+            where TObjectId : BaseObjectId =>
+            await mediator.Publish(domainEvent, cancellationToken);
+
+        public static async Task PublishAsync<TObjectId>(this IMediator mediator, IEnumerable<IDomainEvent<TObjectId>> domainEvents, CancellationToken cancellationToken = default)
+            where TObjectId : BaseObjectId
+        {
+            foreach (var domainEvent in domainEvents)
+            {
+                await mediator.Publish(domainEvent, cancellationToken);
+            }
+        }
+
+        public static async Task PublishAndCommitDomainEventAsync<TObjectId>(this IMediator mediator, IAggregateRoot<TObjectId> aggregate, CancellationToken cancellationToken = default)
+            where TObjectId : BaseObjectId
+        {
+            await mediator.PublishAsync(aggregate.GetUncommittedDomainEvents(), cancellationToken);
+            aggregate.CommitDomainEvents();
+        }
+    }
+}
