3a1a1eba5b6fc9e0ca9f85020676ba966d6dd7b8
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 10:17:39 +0200

    Create new domain event

---
+++ src/POKA.POC.DDD.Domain/DomainEvents/StudentAggregate/StudentEnrolledToCourse.cs
@@ -0,0 +1,26 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+using POKA.POC.DDD.Domain.Exceptions;
+
+namespace POKA.POC.DDD.Domain.DomainEvents
+{
+    public record StudentEnrolledToCourse : BaseDomainEvent<StudentId>
+    {
+        public CourseId CourseId { get; protected set; } = null!;
+
+        private StudentEnrolledToCourse() 
+            : base()
+        {
+        }
+
+        public StudentEnrolledToCourse(StudentId id, int version, CourseId courseId, UserId? authorId = null)
+            : base(id, version, authorId)
+        {
+            if (courseId.HasValue() == false)
+            {
+                throw new AppException(Enums.AppErrorEnum.ArgumentNullPassed, nameof(courseId));
+            }
+
+            CourseId = courseId;
+        }
+    }
+}
