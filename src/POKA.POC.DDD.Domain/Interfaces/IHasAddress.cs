db41909bdb600ecd2f5b65b25070bf070448c051
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 17:20:00 +0200

    Create new interfaces

---
+++ src/POKA.POC.DDD.Domain/Interfaces/IHasAddress.cs
@@ -0,0 +1,9 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+
+namespace POKA.POC.DDD.Domain.Interfaces
+{
+    public interface IHasAddress
+    {
+        Address? Address { get; }
+    }
+}
---
+++ src/POKA.POC.DDD.Domain/Interfaces/IHasCreatedByUserId.cs
@@ -0,0 +1,9 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+
+namespace POKA.POC.DDD.Domain.Interfaces
+{
+    public interface IHasCreatedByUserId
+    {
+        UserId? CreatedByUserId { get; }
+    }
+}
