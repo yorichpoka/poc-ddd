897c00b95eb067014878969c31882f4c06142e2a
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-04 11:15:31 +0200

    Update global using

--- src/POKA.POC.DDD.Application/GlobalUsings.cs
+++ src/POKA.POC.DDD.Application/GlobalUsings.cs
@@ -1,6 +1,7 @@
 ﻿global using POKA.POC.DDD.Domain.ValueObjects;
 global using POKA.POC.DDD.Domain.Aggregates;
 global using POKA.POC.DDD.Domain.Interfaces;
+global using POKA.POC.DDD.Domain.Exceptions;
 global using System.Linq.Expressions;
 global using FluentValidation;
 global using MediatR;
