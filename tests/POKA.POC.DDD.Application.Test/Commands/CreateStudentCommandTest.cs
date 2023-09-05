17b6dc6335d27b93277886cccf0106a38cf1d714
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-05 18:44:00 +0200

    Improve class

--- tests/POKA.POC.DDD.Application.Test/Commands/CreateStudentCommandTest.cs
+++ tests/POKA.POC.DDD.Application.Test/Commands/CreateStudentCommandTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using POKA.POC.DDD.Application.Interfaces;
 using POKA.POC.DDD.Extensions.Commands;
+using POKA.POC.DDD.Domain.ValueObjects;
 using MediatR;
 
 namespace POKA.POC.DDD.Application.Test.Commands
@@ -13,17 +14,31 @@ namespace POKA.POC.DDD.Application.Test.Commands
             // A
             var masterDbRepository = ServiceProvider.GetRequiredService<IMasterDbRepository>();
             var mediator = ServiceProvider.GetRequiredService<IMediator>();
+            var courseId = masterDbRepository.Courses.FirstOrDefaultMappedAsync(l => l.Id).Result;
+            var studentId = default(StudentId);
 
             // A
             masterDbRepository
                 .BeginTransactionAsync()
                 .Wait();
 
-            var command = new CreateStudentCommand("John", "DOE", "john.doe@poka.lu");
-            var _ = mediator.Send(command).Result;
+            // Create student
+            {
+                var students = masterDbRepository.Students.GetAsync().Result;
+                var command = new CreateStudentCommand("John", "DOE", "john.doe@poka.lu");
+                studentId = mediator.Send(command).Result;
+            }
+
+            // Enroll student to course
+            {
+                var command = new EnrollStudentToCourseCommand(studentId, courseId);
+                mediator
+                    .Send(command)
+                    .Wait();
+            }
 
             masterDbRepository
-                .RollbackTransactionAsync()
+                .CommitTransactionAsync()
                 .Wait();
 
             // A
