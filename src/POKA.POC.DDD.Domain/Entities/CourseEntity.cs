6170a2b3d5a59450264642d9365ded978e3a4464
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-05 08:09:52 +0200

    Update entity

--- src/POKA.POC.DDD.Domain/Entities/CourseEntity.cs
+++ src/POKA.POC.DDD.Domain/Entities/CourseEntity.cs
@@ -7,23 +7,18 @@ namespace POKA.POC.DDD.Domain.Entities
     public class CourseEntity : BaseEntity<CourseId>
     {
         public string Name { get; private set; } = null!;
-        public string Description { get; private set; } = null!;
+        public string? Description { get; private set; } = null;
         public short Coefficient { get; private set; }
 
         private CourseEntity()
         {
         }
 
-        public CourseEntity(string name, string description, short coefficient)
+        public CourseEntity(string name, short coefficient, string? description = null)
         {
             if (name.HasValue() == false)
             {
                 throw new AppException(AppErrorEnum.ArgumentNullPassed, nameof(name));
-            }
-
-            if (description.HasValue() == false)
-            {
-                throw new AppException(AppErrorEnum.ArgumentNullPassed, nameof(description));
             }
 
             if (coefficient <= 0)
