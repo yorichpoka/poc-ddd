48095a72bf99d82aa85fd1cf478eff4ebdb99a63
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-05 08:11:06 +0200

    Create and use new entity type configuration

---
+++ src/POKA.POC.DDD.Infrastructure/Persistence/SqlServer/EntityTypeConfiguration/CourseEntityTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace POKA.POC.DDD.Infrastructure.Persistence.SqlServer.EntityTypeConfiguration
+{
+    public class CourseEntityTypeConfiguration : IEntityTypeConfiguration<CourseEntity>
+    {
+        public void Configure(EntityTypeBuilder<CourseEntity> builder)
+        {
+            builder
+                .ConfigureBaseEntity<CourseEntity, CourseId>("Course", "dbo");
+
+            builder
+                .Property(l => l.Name)
+                .HasColumnName("Name")
+                .IsRequired();
+
+            builder
+                .Property(l => l.Coefficient)
+                .HasColumnName("Coefficient")
+                .IsRequired();
+
+            builder
+                .Property(l => l.Description)
+                .HasColumnName("Description")
+                .IsRequired(false);
+        }
+    }
+}
