22b5bc7369ada728ea47371b0dcc9eaa58cc733a
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-05 15:30:24 +0200

    Create new migration

---
+++ src/POKA.POC.DDD.Infrastructure/Persistence/SqlServer/Migrations/202309/Week2/Migration_20230905151459_CreateTableStudent.cs
@@ -0,0 +1,51 @@
+﻿using FluentMigrator;
+
+namespace Edonys.JoyBiiz.Infrastructure.Persistence.Migrations
+{
+    [Migration(20230905151459)]
+    public class Migration_20230905151459_CreateTableStudent : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create
+                .Table("Student")
+                .InSchema("dbo")
+                    // Primary key
+                    .WithColumn("Id")
+                        .AsGuid()
+                        .NotNullable()
+                        .PrimaryKey()
+                        .WithDefault(SystemMethods.NewSequentialId)
+                    // Not nullable
+                    .WithColumn("Version")
+                        .AsInt32()
+                        .NotNullable()
+                    .WithColumn("CreatedOn")
+                        .AsDateTime2()
+                        .NotNullable()
+                        .WithDefault(SystemMethods.CurrentUTCDateTime)
+                    .WithColumn("FirstName")
+                        .AsString(100)
+                        .NotNullable()
+                    .WithColumn("LastName")
+                        .AsString(100)
+                        .NotNullable()
+                    // Nullable
+                    .WithColumn("CreatedByUserId")
+                        .AsGuid()
+                        .Nullable()
+                    .WithColumn("LastUpdatedOn")
+                        .AsDateTime2()
+                        .Nullable()
+                    .WithColumn("Email")
+                        .AsString(100)
+                        .Nullable()
+                    .WithColumn("Address")
+                        .AsString(int.MaxValue)
+                        .Nullable()
+                    .WithColumn("BornOn")
+                        .AsDateTime2()
+                        .Nullable();
+        }
+    }
+}
