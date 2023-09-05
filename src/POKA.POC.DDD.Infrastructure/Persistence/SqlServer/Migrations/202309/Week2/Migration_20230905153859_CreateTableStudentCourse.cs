026aa8c31c49622b171c55391e7416451af51b73
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-05 15:41:55 +0200

    Create new migration class

---
+++ src/POKA.POC.DDD.Infrastructure/Persistence/SqlServer/Migrations/202309/Week2/Migration_20230905153859_CreateTableStudentCourse.cs
@@ -0,0 +1,40 @@
+﻿using FluentMigrator;
+
+namespace Edonys.JoyBiiz.Infrastructure.Persistence.Migrations
+{
+    [Migration(20230905153859)]
+    public class Migration_20230905153859_CreateTableStudentCourse : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create
+                .Table("StudentCourse")
+                .InSchema("dbo")
+                    // Primary key
+                    .WithColumn("Id")
+                        .AsGuid()
+                        .NotNullable()
+                        .PrimaryKey()
+                        .WithDefault(SystemMethods.NewSequentialId)
+                    // Not nullable
+                    .WithColumn("StudentId")
+                        .AsGuid()
+                        .NotNullable()
+                        .ForeignKey(
+                            foreignKeyName: "FK_Student_StudentCourse",
+                            primaryTableSchema: "dbo",
+                            primaryTableName: "Student",
+                            primaryColumnName: "Id"
+                        )
+                    .WithColumn("CourseId")
+                        .AsGuid()
+                        .NotNullable()
+                        .ForeignKey(
+                            foreignKeyName: "FK_Student_StudentCourse",
+                            primaryTableSchema: "dbo",
+                            primaryTableName: "Course",
+                            primaryColumnName: "Id"
+                        );
+        }
+    }
+}
