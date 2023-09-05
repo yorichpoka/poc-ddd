0d94e2263d7dc45615efc3ff791eded0fe0520e7
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-05 15:00:19 +0200

    Create base class of test project

---
+++ tests/POKA.POC.DDD.Application.Test/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using POKA.POC.DDD.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using POKA.POC.DDD.Extensions;
+
+namespace POKA.POC.DDD.Application.Test
+{
+    public abstract class BaseTest
+    {
+        protected IServiceProvider ServiceProvider { get; set; }
+        protected IConfiguration Configuration { get; }
+
+        protected BaseTest()
+        {
+            Configuration = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+
+            ServiceProvider = new ServiceCollection()
+                                    .AddSingleton<IConfiguration>(Configuration)
+                                    .AddDomainAppInfrastructure()
+                                    .BuildServiceProvider();
+
+            var boostrapperService = ServiceProvider.GetRequiredService<IBoostrapperService>();
+
+            boostrapperService.Startup();
+        }
+    }
+}
