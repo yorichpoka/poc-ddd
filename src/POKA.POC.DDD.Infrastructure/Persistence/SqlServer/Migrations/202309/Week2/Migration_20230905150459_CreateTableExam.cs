dc10f1f1767139ed725cdec43994231b6c661f1c
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-05 15:11:32 +0200

    Create new migration classes

---
+++ src/POKA.POC.DDD.Infrastructure/Persistence/SqlServer/Migrations/202309/Week2/Migration_20230905150459_CreateTableExam.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+
+namespace Edonys.JoyBiiz.Infrastructure.Persistence.Migrations
+{
+    [Migration(20230905150459)]
+    public class Migration_20230905150459_CreateTableExam : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create
+                .Table("Exam")
+                .InSchema("dbo")
+                    // Primary key
+                    .WithColumn("Id")
+                        .AsGuid()
+                        .NotNullable()
+                        .PrimaryKey()
+                        .WithDefault(SystemMethods.NewSequentialId)
+                    // Not nullable
+                    .WithColumn("Code")
+                        .AsString(100)
+                        .NotNullable()
+                    .WithColumn("Name")
+                        .AsString(100)
+                        .NotNullable()
+                    .WithColumn("On")
+                        .AsDateTime2()
+                        .NotNullable()
+                    // Nullable
+                    .WithColumn("Description")
+                        .AsString(500)
+                        .Nullable();
+        }
+    }
+}
---
+++ src/POKA.POC.DDD.Infrastructure/Persistence/SqlServer/Migrations/202309/Week2/Migration_20230905151059_CreateTableCourse.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+
+namespace Edonys.JoyBiiz.Infrastructure.Persistence.Migrations
+{
+    [Migration(20230905151059)]
+    public class Migration_20230905151059_CreateTableExam : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create
+                .Table("Course")
+                .InSchema("dbo")
+                    // Primary key
+                    .WithColumn("Id")
+                        .AsGuid()
+                        .NotNullable()
+                        .PrimaryKey()
+                        .WithDefault(SystemMethods.NewSequentialId)
+                    // Not nullable
+                    .WithColumn("Name")
+                        .AsString(100)
+                        .NotNullable()
+                    .WithColumn("Coefficient")
+                        .AsFloat()
+                        .NotNullable()
+                    // Nullable
+                    .WithColumn("Description")
+                        .AsString(500)
+                        .Nullable();
+        }
+    }
+}
