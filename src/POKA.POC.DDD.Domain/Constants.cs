12dbd2ed720f0da7d5547bb1a37e506b5520bf72
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-15 10:01:07 +0200

    Create and use contract resolver

--- src/POKA.POC.DDD.Domain/Constants.cs
+++ src/POKA.POC.DDD.Domain/Constants.cs
@@ -1,9 +1,12 @@
-﻿namespace POKA.POC.DDD.Domain
+﻿using POKA.POC.DDD.Domain.Helpers;
+
+namespace POKA.POC.DDD.Domain
 {
     public static class Constants
     {
         public static readonly JsonSerializerSettings DefaultJsonSerializerSettings = new JsonSerializerSettings
         {
+            ContractResolver = new PrivateResolver(),
             Formatting = Formatting.None,
             MaxDepth = 3
         };
---
+++ src/POKA.POC.DDD.Domain/Helpers/PrivateResolver.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace POKA.POC.DDD.Domain.Helpers
+{
+    public class PrivateResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var jsonProperty = base.CreateProperty(member, memberSerialization);
+
+            if (jsonProperty.Writable == false)
+            {
+                var propertyInfo = member as PropertyInfo;
+                var hasPrivateSetter = propertyInfo?.GetSetMethod(true) != null;
+
+                jsonProperty.Writable = hasPrivateSetter;
+            }
+
+            return jsonProperty;
+        }
+    }
+}
