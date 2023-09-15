a2fdfe5d92bb60a1c83142dfcae9cd89ae246457
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-15 10:22:15 +0200

    Create new interface

---
+++ src/POKA.POC.DDD.Application/Common/Interfaces/Repositories/IRequestRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace POKA.POC.DDD.Application.Interfaces
+{
+    public interface IRequestRepository
+    {
+        Task<int> CountAsync(Expression<Func<RequestEntity, bool>> predicate, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Expression<Func<RequestEntity, bool>> predicate, CancellationToken cancellationToken = default);
+        Task UpdateAsync(RequestId requestId, RequestEntity request, CancellationToken cancellationToken = default);
+        Task<RequestEntity> InitializeAsync(IBaseRequest request, CancellationToken cancellationToken = default);
+    }
+}
