9debfef2dd5b0fa378679253c8d133232413700c
Ulrich POKA <ulrich.poka@technologypartner.io>
2023-09-15 10:28:03 +0200

    Create new entity and his object id

---
+++ src/POKA.POC.DDD.Domain/Entities/EventEntity.cs
@@ -0,0 +1,14 @@
+﻿using POKA.POC.DDD.Domain.ValueObjects;
+
+namespace POKA.POC.DDD.Domain.Entities
+{
+    public class EventEntity : BaseEntity<EventId>
+    {
+        public string AggregateId { get; set; } = null!;
+        public string AggregateType { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public int Version { get; set; }
+        public string Data { get; set; } = null!;
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ src/POKA.POC.DDD.Domain/ValueObjects/ObjectIds/EventId.cs
@@ -0,0 +1,14 @@
+﻿namespace POKA.POC.DDD.Domain.ValueObjects
+{
+    public record EventId : BaseObjectId
+    {
+        protected override string _type => "event";
+
+        public EventId(Guid value)
+            : base(value)
+        {
+        }
+
+        public override string ToString() => base.ToString();
+    }
+}
